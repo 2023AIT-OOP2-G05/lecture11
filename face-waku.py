aabf497ffde0b204bb41894caf8f02033fd874c0
harumitsu1234 <harumitsu1211@gmail.com>
2023-12-21 16:20:46 +0900

    色々変更

---
+++ face-waku.py
@@ -0,0 +1,33 @@
+import cv2
+import os
+def face(inputImgPath):
+    #画像読み込み
+    img = cv2.imread(inputImgPath)
+    #グレースケール化
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+    # 学習済みモデルの読み込み
+    cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    # 顔を検出する
+    #lists = cascade.detectMultiScale(img_gray, minSize=(100, 100))
+    faces = cascade.detectMultiScale(img, scaleFactor=1.1,minNeighbors=5, minSize=(30,30))
+    #赤枠処理、表示
+    if len(faces):
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), thickness=2)
+            cv2.imshow('img', img)
+            cv2.waitKey(0)
+    else:
+        print('Nothing')
+
+    print("OK")
+    imgName =os.path.basename(inputImgPath)
+    imgName =imgName.split('.')
+    filename ="../画像保存先/ " +imgName[0]  + "_exceptBlue.jpg"
+    #print(filename)
+    cv2.imwrite(filename, img)
+
+if __name__ == "__main__":
+    app = face()
+    app.run()
