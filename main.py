5868b22c5f789de2091875f7a5a7a396709fe698
ManayaT <waddle-dee.1210@docomo.ne.jp>
2023-12-21 15:03:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app.config["JSON_AS_ASCII"] = False
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("Image_Processing.html")
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
